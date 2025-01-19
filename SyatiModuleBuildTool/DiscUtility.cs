c14a5f3f51503699282fdfc340435375c9a1de95
VTXG <VTXG@users.noreply.github.com>
2025-01-19 18:08:12 +0000

    Add warning when overwriting file

--- SyatiModuleBuildTool/DiscUtility.cs
+++ SyatiModuleBuildTool/DiscUtility.cs
@@ -7,20 +7,23 @@ public static class DiscUtility {
     }
 
     public static void CopyFiles(ModuleInfo module, string output) {
-        var sourceDiscFolder = Path.Combine(module.FolderPath, "disc");
+        var discFolder = Path.Combine(module.FolderPath, "disc");
 
-        if (!Directory.Exists(sourceDiscFolder))
+        if (!Directory.Exists(discFolder))
             return;
 
-        Console.WriteLine($"Copying files from {sourceDiscFolder}");
+        Console.WriteLine($"Copying files from {discFolder}");
 
-        var discPaths = Directory.GetFiles(sourceDiscFolder, "*", SearchOption.AllDirectories);
+        var discPaths = Directory.GetFiles(discFolder, "*", SearchOption.AllDirectories);
 
         foreach (var sourcePath in discPaths) {
-            var relativePath = Path.GetRelativePath(sourceDiscFolder, sourcePath);
+            var relativePath = Path.GetRelativePath(discFolder, sourcePath);
             var targetPath = Path.Combine(output, relativePath);
 
             try {
+                if (Path.Exists(targetPath))
+                    Console.WriteLine($" - File will replace {targetPath}");
+
                 var targetDirectory = Path.GetDirectoryName(targetPath);
                 if (targetDirectory is not null)
                     Directory.CreateDirectory(targetDirectory);
@@ -28,7 +31,7 @@ public static class DiscUtility {
                 File.Copy(sourcePath, targetPath, true);
             }
             catch (Exception e) {
-                Console.WriteLine($"Error while copying \"{sourceDiscFolder}\": {e.Message}");
+                Console.WriteLine($"Error while copying {discFolder}: {e.Message}");
             }
         }
     }
